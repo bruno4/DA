4dd02da2636d264397c0bbe35014043c382a5bd8
danielmf123 <danimadurof@gmail.com>
2016-06-02 17:54:48 +0100

    Add files via upload

--- Bruno_Daniel_Stand/formAdCarroAluguer.Designer.cs
+++ Bruno_Daniel_Stand/formAdCarroAluguer.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAdCarroAluguer));
             this.txtMatricula = new System.Windows.Forms.TextBox();
             this.txtNChassis = new System.Windows.Forms.TextBox();
             this.txtModelo = new System.Windows.Forms.TextBox();
@@ -180,6 +181,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "formAdCarroAluguer";
